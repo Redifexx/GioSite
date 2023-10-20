c55217b66dd0af8b33a337754d3d1a731c608652
Gio <gohan1703@gmail.com>
2023-10-20 11:03:43 -0400

    bigsize

---
+++ gio-site/src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="main_">
+      <div className="left_" >
+        <div className="titleBox_">
+          <h1 className="myName_">
+            HELLO
+          </h1>
+        </div>
+      </div>
+      <div className="right_">
+         
+      </div>
+    </main>
+  )
+}
